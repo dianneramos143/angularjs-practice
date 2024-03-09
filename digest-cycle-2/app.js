88af0baf66d48f03c9cf1f8a8667860661f3452e
Dianne Ramos <dianne.ramos@herma.com.ph>
2024-03-09 15:49:59 +0800

    add more files

--- digest-cycle-2/app.js
+++ digest-cycle-2/app.js
@@ -1,4 +1,4 @@
-(function () {
+(function (){
 'use script';
 
 angular.module('CounterApp', [])
@@ -7,35 +7,35 @@ angular.module('CounterApp', [])
 CounterController.$inject = ['$scope', '$timeout'];
 
 function CounterController ($scope, $timeout) {
-
     $scope.counter = 0;
 
-    $scope.upCounter = function () {
-        $timeout(function (){
-            $scope.counter++;
-            console.log("Counter incremented");
-        }, 2000);
-    };
-
-    // Implementing $apply
-    // $scope.upCounter = function () {
-    //     setTimeout(function () {
-    //         $scope.$apply(function () {
-    //             $scope.counter++;
-    //             console.log("Counter incremented");
-    //         });
-    //     },2000);
-    // };
-
-
-    // Implementing $digest -- 
+    //Implementing $digest
     // $scope.upCounter = function () {
     //     setTimeout(function () {
     //         $scope.counter++;
-    //         console.log("Counter incremented");
+    //         console.log("Counter incremented. Implemented using $digest");
     //         $scope.$digest();
-    //     },2000);
+    //     }, 2000);
+    // }
+
+    //Implementing $apply
+    // $scope.upCounter = function(){
+    //     setTimeout(function (){
+    //         $scope.$apply( function(){
+    //             $scope.counter++;
+    //             console.log("Counter incremented. Implemented using $apply.");
+    //         });
+    //     }, 2000);
     // };
+
+    //Implementing Angular-specific function
+    $scope.upCounter = function () {
+        $timeout(function () {
+            $scope.counter++;
+            console.log("Counter incremented. Implemented using Angular-specific function.")
+        },2000);
+    };
 }
+
 
 })();
